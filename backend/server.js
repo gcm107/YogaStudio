a392eb5d0f53d6b15520fa64c059ef4fcf883411
Cale McDowell <grasonmcdowell@gmail.com>
2025-09-18 00:27:25 -0500

    definitely screwed up the landing page. trying fix of the html path.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -10,9 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// testing to make sure it runs
+// using path for the front end
+app.use(express.static(path.join(__dirname, '..')));
+
+// testing fix to see if the path to the front end is right now.
 app.get('/', (req, res) => {
-  res.json({ message: 'Yoga Studio API is running!' });
+  res.sendFile(path.join(__dirname, '../frontend.html'));
 });
 
 // routes
