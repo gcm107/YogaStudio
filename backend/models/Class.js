767826af5511529378f9355b5471ec962eaf4671
Cale <grasonmcdowell@gmail.com>
2025-09-24 09:54:55 -0500

    Refactored instructor and class schemas so it is easier to read.

--- backend/models/Class.js
+++ backend/models/Class.js
@@ -1,40 +1,32 @@
 const mongoose = require('mongoose');
 
-// schema for class 
+// schema for class collection in mongo
+// based on canvas instructions and use cases.
 const classSchema = new mongoose.Schema({
+
+  // using the I_ID from the instructor collection in mongoose
+  // for the dropdown in frontend
   I_ID: {type: String, required: true},
 
-  day: {
-    type: String,
-    required: true
-  },
+  day: {type: String, required: true},
 
-  time: {
-    type: String,
-    required: true
-  },
+  time: {type: String,required: true},
 
-  classType: {
-    type: String,
-    enum: ['General', 'Special'],
-    required: true
-  },
+  // from canvas instructions, type of class
+  classType: {type: String, enum: ['General', 'Special'], required: true},
 
-  specialClassDescription: {
-    type: String,
-    required: function() { return this.classType === 'Special'; }
-  },
+  specialClassDescription: {type: String, required: function() { return this.classType === 'Special'; }},
 
-  payRate: {
-    type: Number,
-    required: true
-  },
-  isPublished: {
-    type: Boolean,
-    default: false
-  }
-}, {
-  timestamps: true
-});
+  payRate: {type: Number, required: true},
 
+  isPublished: {type: Boolean, default: false},
+
+}, 
+
+// timestamp for when the class was added
+{timestamps: true}
+
+); 
+
+// exporting so we can use it
 module.exports = mongoose.model('Class', classSchema);
--- backend/models/Instructor.js
+++ backend/models/Instructor.js
@@ -1,45 +1,32 @@
+// instructor schema for mongodb
 const mongoose = require('mongoose');
 
+// constructing the schema 
 const instructorSchema = new mongoose.Schema({
 
+  // instructions on canvas tell us to use I_ID
   I_ID: {type: String, unique: true},
 
   firstName: {type: String, required: true, trim: true},
 
-  lastName: {
-    type: String,
-    required: true,
-    trim: true
-  },
+  lastName: {type: String, required: true, trim: true},
 
-  address: {
-    type: String,
-    required: true
-  },
+  address: {type: String, required: true},
 
-  phone: {
-    type: String,
-    required: true
-  },
+  phone: {type: String, required: true},
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
+  email: {type: String, required: true, unique: true, lowercase: true, trim: true},
 
-  preferredCommunication: {
-    type: String,
-    enum: ['phone', 'email'],
-    required: true
-  }
+  preferredCommunication: {type: String, enum: ['phone', 'email'], required: true},
+
 }, 
 
-{ timestamps: true}
+// timestamp for when it was created or updated
+{timestamps: true}
+
 );
 
+// exporting so we can use it
 module.exports = mongoose.model(
   'Instructor', instructorSchema
 );
